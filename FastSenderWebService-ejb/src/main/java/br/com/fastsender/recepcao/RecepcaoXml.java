df287f77aebfbb854f8eb3c2bb4b1c42168c1e6c
fastsender <fastsendergit@gmail.com>
2018-07-15 16:27:30 -0300

    Algumas implementações - em andamento

---
+++ FastSenderWebService-ejb/src/main/java/br/com/fastsender/recepcao/RecepcaoXml.java
@@ -0,0 +1,56 @@
+package br.com.fastsender.recepcao;
+
+import br.com.fastsender.mongo.connection.MongoDBConnection;
+import br.com.fastsender.mongo.facade.XmlFacade;
+import br.com.fastsender.utils.EasyString;
+import com.mongodb.DB;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lucas
+ */
+public class RecepcaoXml {
+    
+    private static final MongoDBConnection MONGODB_CONNECTION = new MongoDBConnection();
+    private static final DB dbMongo = MONGODB_CONNECTION.getDB();
+    // Galera, essa classe aqui foi só pra testes, mas se precisar 
+    // tem uns métodos aqui que da pra usar
+    private String xmlReceb;
+    EasyString parse = new EasyString();
+    
+    private XmlFacade xmlFacade = new XmlFacade();
+    public void recuperaXml() {
+        this.xmlReceb = parse.fileTxtToString("/home/lucas/Documents/FastSender/exemplo.xml");
+    }
+    
+    public static void main(String[] args) {
+        RecepcaoXml xml = new RecepcaoXml();
+        xml.startXml4Mongo();
+    }
+
+    //inicia a gravação do xml no mongo DB, onde vai ter um método recursivo de teste
+    public void startXml4Mongo() {
+        boolean send = true;
+        boolean popula = true;
+        int count = 0;
+        List<String> listXml = new ArrayList<>();
+        recuperaXml();
+        while (send) {
+            while(popula){
+                listXml.add(xmlReceb);
+                count++;
+                if (count == 100){
+                    popula = false;
+                }
+                
+            }
+            xmlFacade.getXmlBo().inserirXml(dbMongo, listXml);
+        }
+        if (count == 100){
+            send = false;
+            count = 0;
+        }
+        System.out.println("Inserido com sucesso");
+    }
+}
